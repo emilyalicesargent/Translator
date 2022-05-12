e60f39029a8b18dd1ce8205ce3796eb00f5392f3
Emily Sargent <emilyalicesargent@hotmail.co.uk>
2022-05-12 15:43:38 +0100

    Translator

---
+++ script.js
@@ -0,0 +1,51 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchangeIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
+
+
+selectTag.forEach((tag, id) => {
+    for (const country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB") {
+            selected = "selected";
+        } else if(id == 1 && country_code == "hi-IN") {
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value;
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        });
+    });
+
+icons.forEach(icons => {
+    icons.addEventListener("click", ({target}) => {
+            if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } 
+    });
+});
